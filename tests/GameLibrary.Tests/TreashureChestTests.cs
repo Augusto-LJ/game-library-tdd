c336201d6585052878f10a053c9969ddd4671081
Augusto-LJ <augustolimajardim@gmail.com>
2026-01-18 19:44:55 -0300

    feat: adds all test cases for TreasureChest

--- tests/GameLibrary.Tests/TreashureChestTests.cs
+++ tests/GameLibrary.Tests/TreashureChestTests.cs
@@ -3,14 +3,60 @@ namespace GameLibrary.Tests;
 public class TreashureChestTests
 {
     [Fact]
-    public void CanOpenTest()
+    public void CanOpen_ChestIsLockedAndHasKey_ReturnsTrue()
     {
         // Arrange
-        bool isOpen = true;
-        var chest = new TreasureChest(isOpen);
+        bool isLocked = true;
+        bool hasKey = true;
+        var sut = new TreasureChest(isLocked);
 
         // Act
-        var result = chest.CanOpen(isOpen);
+        var result = sut.CanOpen(hasKey);
+
+        // Assert
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void CanOpen_ChestIsLockedAndHasNoKey_ReturnsFalse()
+    {
+        // Arrange
+        bool isLocked = true;
+        bool hasKey = false;
+        var sut = new TreasureChest(isLocked);
+
+        // Act
+        var result = sut.CanOpen(hasKey);
+
+        // Assert
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void CanOpen_ChestIsUnlockedAndHasKey_ReturnsTrue()
+    {
+        // Arrange
+        bool isLocked = false;
+        bool hasKey = true;
+        var sut = new TreasureChest(isLocked);
+
+        // Act
+        var result = sut.CanOpen(hasKey);
+
+        // Assert
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void CanOpen_ChestIsUnlockedAndHasNoKey_ReturnsTrue()
+    {
+        // Arrange
+        bool isLocked = false;
+        bool hasKey = false;
+        var sut = new TreasureChest(isLocked);
+
+        // Act
+        var result = sut.CanOpen(hasKey);
 
         // Assert
         Assert.True(result);
