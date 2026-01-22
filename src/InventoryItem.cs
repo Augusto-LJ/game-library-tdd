610135b41c7f967561e58131fa9b6c1f645ed554
Augusto-LJ <augustolimajardim@gmail.com>
2026-01-21 22:40:27 -0300

    Asserting objects

--- src/InventoryItem.cs
+++ src/InventoryItem.cs
@@ -1,6 +1,8 @@
 ﻿namespace GameLibrary;
 
-public class InventoryItem(string description)
+public class InventoryItem(int id, string name, string description)
 {
+    public int Id { get; private set; } = id;
+    public string Name { get; private set; } = name;
     public string Description { get; private set; } = description;
 }
---
+++ tests/GameLibrary.Tests/PlayerExtensionsTests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+
+namespace GameLibrary.Tests;
+
+public class PlayerExtensionsTests
+{
+    [Fact]
+    public void ToDto_WhenCalled_MapsProperties()
+    {
+        // Arrange
+        var sut = new Player("Augusto", 1, DateTime.Now);
+        var item = new InventoryItem(101, "Sword", "A sharp blade");
+        sut.AddItemToInventory(item);
+
+        // Act
+        var dto = sut.ToDto();
+
+        // Assert
+        dto.Should().BeEquivalentTo(sut, options => options
+                        .ExcludingMissingMembers());
+    }
+}
