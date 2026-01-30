a97ad64da1a74c48bece9d6582af16faa5237ed3
Augusto-LJ <augustolimajardim@gmail.com>
2026-01-29 21:10:55 -0300

    Adds tests using booleans.
    (Avoid testing implementation details)

---
+++ src/QuestLog.cs
@@ -0,0 +1,12 @@
+﻿namespace GameLibrary;
+
+public class QuestLog(string inicialQuest)
+{
+    public List<string> Quests { get; private set; } = [inicialQuest];
+
+    public bool AddQuest(string quest)
+    {
+        Quests.Add(quest);
+        return true;
+    }
+}
---
+++ tests/GameLibrary.Tests/QuestLogTests.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+
+namespace GameLibrary.Tests;
+public class QuestLogTests
+{
+    [Fact]
+    public void AddQuest_WhenCalled_AddsQuestLog()
+    {
+        // Arrange
+        var sut = new QuestLog("Initial Quest");
+
+        // Act
+        var result = sut.AddQuest("Second Quest");
+
+        // Assert
+        result.Should().Be(true);
+        sut.Quests.Should().HaveCount(2);
+        sut.Quests.Should().Contain("Second Quest");
+    }
+}
